37ab16e980e3df38e7bd1402e3e07859c49fc62d
sericaer <sericaer@outlook.com>
2023-01-31 22:14:33 +0800

    dynamic load sprite from StreamingAssets

--- UnityProject/Assets/Scenes/MainScene/MainScene.cs
+++ UnityProject/Assets/Scenes/MainScene/MainScene.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.Tilemaps;
@@ -13,6 +14,19 @@ public class MainScene : MonoBehaviour
 
     public List<Building> buildings;
 
+    private Sprite[] sprites;
+
+    private void Awake()
+    {
+        sprites = Directory.EnumerateFiles(Application.streamingAssetsPath, "*.png")
+            .Select(imgPath =>
+            {
+                Texture2D tex = new Texture2D(2, 2);
+                tex.LoadImage(File.ReadAllBytes(imgPath));
+                var sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(0, 0), 64);
+                return sprite;
+            }).ToArray();
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -29,6 +43,7 @@ public class MainScene : MonoBehaviour
     {
         var buildingPlan = Instantiate<BuildingPlan>(buildingPlanPrototype);
         buildingPlan.tilemap = tilemap;
+        buildingPlan.sprite = sprites[0];
 
         buildingPlan.confirmLocalEvent.AddListener((pos) =>
         {
@@ -40,6 +55,7 @@ public class MainScene : MonoBehaviour
                 var building = Instantiate<Building>(buildingPrototype);
                 building.tilemap = tilemap;
                 building.cellPos = pos;
+                building.sprite = sprites[0];
 
                 buildings.Add(building);
             }
