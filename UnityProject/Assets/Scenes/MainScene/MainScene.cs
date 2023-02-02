853aa840a526b549d278f359a73a5017c5a5c397
sericaer <sericaer@outlook.com>
2023-02-02 22:42:19 +0800

    change game resources

--- UnityProject/Assets/Scenes/MainScene/MainScene.cs
+++ UnityProject/Assets/Scenes/MainScene/MainScene.cs
@@ -8,7 +8,17 @@ using UnityEngine.Tilemaps;
 public class MainScene : MonoBehaviour
 {
     public Tilemap tilemap;
-
+    public TileBase tileset;
+    private void Start()
+    {
+        for(int i=-50; i<=50; i++)
+        {
+            for(int j=-50; j<=50; j++)
+            {
+                tilemap.SetTile(new Vector3Int(i, j), tileset);
+            }
+        }
+    }
     // Update is called once per frame
     void Update()
     {
