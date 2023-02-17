57dafdbb5f82a754a2787675ab04d103d531d29d
sericaer <sericaer@outlook.com>
2023-02-17 16:59:10 +0800

    Add Plugins/RuntimeGUITable

---
+++ UnityProject/Assets/Scenes/MainScene/BuildingForm.cs
@@ -0,0 +1,26 @@
+﻿using RxPropertyChanged;
+using Sessions;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildingForm : MonoBehaviour
+{
+    public List<BuildingFormItem> items;
+
+    //protected override void BindingInit()
+    //{
+    //    Binding(dataContext => dataContext.count, buildingCount);
+    //}
+
+    private void Awake()
+    {
+        
+    }
+}
+
+[System.Serializable]
+public class BuildingFormItem
+{
+    public string name;
+}
